528ed392ef2ce2fad8f7ebcdd0694583b8bbe223
Aleks Anatoliyovich <44201618+Aleks0010V@users.noreply.github.com>
2020-11-06 16:55:42 +0200

    docker compose

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
+import os
+
 from sqlalchemy import create_engine
 from databases import Database
 
-HOST = 'mysql://root:123456@localhost:3306/test_db'
+HOST = f'mysql://root:123456@{os.environ.get("DB_HOST", "127.0.0.1")}:3306/test_db'
 engine = create_engine(HOST)
 db = Database(HOST)
