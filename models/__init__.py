334d31424a876a54b9cfac49ae1870dc2c838fda
Aleks Anatoliyovich <44201618+Aleks0010V@users.noreply.github.com>
2020-11-13 21:32:27 +0200

    database refactoring

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .user import tokens_table, users_table, roles_table, UserInfo, UserInDB, SecuredUserInfo, Login, Token, \
-    NewUser, NewUserResponse
-from .schedule import schedule_table, Appointment, get_all_unavailable_time_slots_query, \
-    get_user_time_slots
-
-__all__ = ['tokens_table', 'users_table', 'roles_table', 'schedule_table',
-           'UserInDB', 'UserInfo', 'SecuredUserInfo', 'Login', 'Token', 'NewUser', 'NewUserResponse',
-           'Appointment', 'get_all_unavailable_time_slots_query', 'get_user_time_slots']
